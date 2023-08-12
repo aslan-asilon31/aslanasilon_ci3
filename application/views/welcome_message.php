7c2497b385611a041bc750cbe51cb74dcad525c9
aslan-asilon31 <sulaslansetiawan31@gmail.com>
2023-08-12 14:31:30 +0700

    update file welcome view

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,1 +1,13 @@
-welcome aslanasilon ci_3
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Welcome</title>
+</head>
+<body>
+
+welcome to aslanasilon CI 3.
+	
+</body>
+</html>
