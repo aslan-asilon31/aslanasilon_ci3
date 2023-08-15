f0fbaad2e469b8541b3995a394ef26f109b05ca9
aslan-asilon31 <sulaslansetiawan31@gmail.com>
2023-08-15 08:12:57 +0700

    update assets and welcome page

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://aslanasilon-ci3.aslanasilon.com/';
+$config['base_url'] = 'http://localhost/aslanasilon_ci3//';
 
 /*
 |--------------------------------------------------------------------------
